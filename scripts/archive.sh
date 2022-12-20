a8762ee37a0c2360117dee2e23e7cd85c4db635b
sandip.kalsait <kalsaitsandip91@gmail.com>
2022-12-20 15:27:45 +0530

    archive and remove files functionality has been added

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -1,9 +1,41 @@
 #!/usr/bin/env bash
+source ./config/config.shlib;
 
-source /config/config.shlib;
+# echo "Start Archive ..."
+path="$(config_get FILE_PATH)"
+# home="$(config_get HOME_PATH)"
+ext="$(config_get ARCHIVE_FILE_EXT)"
 
-$Ext
-$AGE_OF_FILE
-$PATH
+# echo $ext
 
-tar cvf $PATH/*.$Ext.tgz $PATH/*.$Ext
+$files
+
+
+
+declare -a FILES
+
+#Get all files orlder than x days
+
+for file in $(find .$path -type f -name "*.$ext")
+do
+    FILES=(${FILES[*]} "$file")
+done
+
+
+#check length of an file array
+arrayLength=${#FILES[@]}
+
+
+for ((i=0; i<${arrayLength}; i++))
+do
+    tar -uvf .$path"/log_$(date "+%d-%m-%Y")".tar  ${FILES[$i]}
+    sleep 5 
+done
+
+
+
+# echo "End Archive ..."
+
+
+
+
--- scripts/remove.sh
+++ scripts/remove.sh
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
 
-source /config/config.shlib;
+source ./config/config.shlib;
 
-$Ext
-$AGE_OF_FILE
-$PATH
+# echo "Start Archive ..."
+path="$(config_get FILE_PATH)"
+# home="$(config_get HOME_PATH)"
+ext="$(config_get LOG_FILE_EXT)"
 
-find $PATH -type f -mtime +$AGE_OF_FILE -name "*.$Ext" -exec rm -rf {} \;
+AGE_OF_LOG_FILE="$(config_get AGE_OF_LOG_FILE)"
+AGE_OF_ARCHIVE_FILE="$(config_get AGE_OF_ARCHIVE_FILE)"
+
+
+echo $AGE_OF_ARCHIVE_FILE
+echo $AGE_OF_LOG_FILE
+
+
+echo "remove file log files"
+
+#find $path -type f -mmin +$AGE_OF_FILE -name "*.$ext" -exec rm -rf {} \;
+pwd
+#delete all log files older than $AGE_OF_LOG_FILE  days
+find $(pwd)$path -type f -mtime +$AGE_OF_LOG_FILE -name "*.$ext" -exec rm -rf {} \;
+
+#delete all archive files older than $AGE_OF_ARCHIVE_FILE  days
+find $(pwd)$path -type f -mtime +$AGE_OF_ARCHIVE_FILE -name "*.tar" -exec rm -rf {} \;
+
+
